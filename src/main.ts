01274b91f648ec5d356d9a6db724faf91ee492f6
Pal Nguyen <89907085+palnguyen20-01@users.noreply.github.com>
2024-06-20 10:19:15 +0700

    refactor(login-account): read origin CORS from .env

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { PORT } from './shared/constants';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: '*',
     credentials: true,
+    origin: `http://${configService.get<string>('DOMAIN')}`,
   });
 
   await app.listen(PORT);
