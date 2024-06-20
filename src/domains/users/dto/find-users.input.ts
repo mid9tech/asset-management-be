5f078465ee3463f93ec7c6f093e1e02e06cf6cc7
hoangphuc28 <zhoangp@gmail.com>
2024-06-20 15:45:33 +0700

    refactor(findUser-dto): change graphql user type

--- src/domains/users/dto/find-users.input.ts
+++ src/domains/users/dto/find-users.input.ts
@@ -6,7 +6,6 @@ import { BasePagingRequest } from 'src/shared/generics';
 @InputType()
 export class FindUsersInput extends BasePagingRequest {
   @Field(() => String, { nullable: true })
-  @IsEnum(USER_TYPE)
   type: USER_TYPE;
 
   @Field(() => String, { nullable: true, defaultValue: 'firstName' })
